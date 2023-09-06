fe9c9443c60dfd81a212d0098e42306cb80bc9b1
arushijhalani <143315092+arushijhalani@users.noreply.github.com>
2023-09-07 00:32:26 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 import { solve, insertValues, populateValues } from './solver.js'
 import { drawBoard } from './draw_board.js'
-//import { loadRandomBoard } from './load_boards.js'
+
 
 const solveButton = document.querySelector("#solve-button")
 const clearButton = document.querySelector("#clear-button")
-//const loadButton = document.querySelector('#load-button')
+
 
 function main() {
     drawBoard()
@@ -17,6 +17,6 @@ function main() {
         }
     })
     clearButton.addEventListener('click', () => window.location.reload(true))
-    //loadButton.addEventListener('click', () => loadRandomBoard())
+   
 }
 main()
